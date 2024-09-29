ed1c13510e06fd15d927978a0ce9ffa8aa1e829b
mkx419 <mkx419@gmail.com>
2024-09-30 02:03:11 +0900

    revert: preparing to make vite plugin independent

--- packages/rollup/src/index.ts
+++ packages/rollup/src/index.ts
@@ -1,23 +1,14 @@
-import { join, relative } from "path";
-import { type Plugin } from "rollup";
-
 import { transform } from "@mkx419/tx/transform";
+import { type Plugin } from "rollup";
 
 export function txPlugin(): Plugin<never> {
   return {
     name: "tx",
     transform: {
       order: "post",
-      async handler(code, id) {
-        if (/\.[cm]?[jt]sx?|vue|svelte$/.test(id)) {
-          const resolution = await this.resolve("@mkx419/tx", undefined, {});
-
-          if (resolution?.id) {
-            return transform(code, {
-              fileName: id,
-              moduleName: join("/", relative(__dirname, resolution.id)).replace(/\\/g, "/"),
-            });
-          }
+      handler(code, id) {
+        if (/\.[jt]sx?|vue|svelte$/.test(id)) {
+          return transform(code, { fileName: id });
         }
       },
     },
