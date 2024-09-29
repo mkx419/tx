d056becd109a964c32c6f635066220247dc563c1
mkx419 <mkx419@gmail.com>
2024-09-30 02:05:23 +0900

    feat: commonjs support

--- packages/tx/tsup.config.ts
+++ packages/tx/tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts", "src/transform.ts"],
-  format: "esm",
+  format: ["esm", "cjs"],
   dts: true,
   clean: true,
 });
